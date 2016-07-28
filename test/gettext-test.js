20a630a3c7b6eef159b2a0d3290778c72abb06be
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-07-28 11:26:35 -0700

    Fix locale vs domain confusion
    A domain is a module that is translated independently, and has
    nothing to do with a language code.
    Fix this fundamental misinterpretation of how gettext works.

--- test/gettext-test.js
+++ test/gettext-test.js
@@ -9,45 +9,39 @@ chai.config.includeStack = true;
 
 describe('Gettext', function() {
 
-    describe('#_normalizeDomain', function() {
-        it('should normalize domain key', function() {
-            var gt = new Gettext();
-
-            expect(gt._normalizeDomain('ab-cd_ef.utf-8')).to.equal('ab_CD-EF');
-            expect(gt._normalizeDomain('ab-cd_ef', true)).to.equal('ab');
-        });
-    });
-
     describe('#addTextdomain', function() {
 
         it('Should add from a mo file', function() {
             var gt = new Gettext();
+            gt.setlocale('et-EE');
             var moFile = fs.readFileSync(__dirname + '/fixtures/latin13.mo');
 
-            gt.addTextdomain('et-EE', moFile);
+            gt.addTextdomain('messages', moFile);
 
-            expect(gt.domains.et_EE).to.exist;
-            expect(gt.domains.et_EE.charset).to.equal('iso-8859-13');
+            expect(gt.domains.messages).to.exist;
+            expect(gt.domains.messages.charset).to.equal('iso-8859-13');
         });
 
         it('Should add from a po file', function() {
             var gt = new Gettext();
+            gt.setlocale('et-EE');
             var poFile = fs.readFileSync(__dirname + '/fixtures/latin13.po');
 
-            gt.addTextdomain('et-EE', poFile);
+            gt.addTextdomain('messages', poFile);
 
-            expect(gt.domains.et_EE).to.exist;
-            expect(gt.domains.et_EE.charset).to.equal('iso-8859-13');
+            expect(gt.domains.messages).to.exist;
+            expect(gt.domains.messages.charset).to.equal('iso-8859-13');
         });
 
         it('Should add from a json file', function() {
             var gt = new Gettext();
+            gt.setlocale('et-EE');
             var jsonFile = JSON.parse(fs.readFileSync(__dirname + '/fixtures/latin13.json'));
 
-            gt.addTextdomain('et-EE', jsonFile);
+            gt.addTextdomain('messages', jsonFile);
 
-            expect(gt.domains.et_EE).to.exist;
-            expect(gt.domains.et_EE.charset).to.equal('iso-8859-13');
+            expect(gt.domains.messages).to.exist;
+            expect(gt.domains.messages.charset).to.equal('iso-8859-13');
         });
 
     });
@@ -58,10 +52,10 @@ describe('Gettext', function() {
             var moFile = fs.readFileSync(__dirname + '/fixtures/latin13.mo');
 
             expect(gt.textdomain()).to.be.false;
-            gt.addTextdomain('et-EE', moFile);
-            expect(gt.textdomain()).to.equal('et_EE');
-            gt.addTextdomain('cd-EE', moFile);
-            expect(gt.textdomain()).to.equal('et_EE');
+            gt.addTextdomain('messages', moFile);
+            expect(gt.textdomain()).to.equal('messages');
+            gt.addTextdomain('messages', moFile);
+            expect(gt.textdomain()).to.equal('messages');
         });
 
         it('should change default domain', function() {
@@ -69,12 +63,12 @@ describe('Gettext', function() {
             var moFile = fs.readFileSync(__dirname + '/fixtures/latin13.mo');
 
             expect(gt.textdomain()).to.be.false;
-            gt.addTextdomain('et-EE', moFile);
-            expect(gt.textdomain()).to.equal('et_EE');
-            gt.addTextdomain('cd-EE', moFile);
-            expect(gt.textdomain()).to.equal('et_EE');
-            gt.textdomain('cd_EE');
-            expect(gt.textdomain()).to.equal('cd_EE');
+            gt.addTextdomain('messages', moFile);
+            expect(gt.textdomain()).to.equal('messages');
+            gt.addTextdomain('domain1', moFile);
+            expect(gt.textdomain()).to.equal('messages');
+            gt.textdomain('domain2');
+            expect(gt.textdomain()).to.equal('domain2');
         });
     });
 
@@ -83,37 +77,38 @@ describe('Gettext', function() {
 
         beforeEach(function() {
             gt = new Gettext();
+            gt.setlocale('et-EE');
             var poFile = fs.readFileSync(__dirname + '/fixtures/latin13.po');
-            gt.addTextdomain('et-EE', poFile);
+            gt.addTextdomain('messages', poFile);
         });
 
         describe('#dnpgettext', function() {
             it('should return default singular', function() {
-                expect(gt.dnpgettext('et_EE', '', '0 matches', 'multiple matches', 1)).to.equal('0 matches');
+                expect(gt.dnpgettext('messages', '', '0 matches', 'multiple matches', 1)).to.equal('0 matches');
             });
 
             it('should return default plural', function() {
-                expect(gt.dnpgettext('et_EE', '', '0 matches', 'multiple matches', 100)).to.equal('multiple matches');
+                expect(gt.dnpgettext('messages', '', '0 matches', 'multiple matches', 100)).to.equal('multiple matches');
             });
 
             it('should return singular match from default context', function() {
-                expect(gt.dnpgettext('et_EE', '', 'o2-1', 'o2-2', 1)).to.equal('t2-1');
+                expect(gt.dnpgettext('messages', '', 'o2-1', 'o2-2', 1)).to.equal('t2-1');
             });
 
             it('should return plural match from default context', function() {
-                expect(gt.dnpgettext('et_EE', '', 'o2-1', 'o2-2', 2)).to.equal('t2-2');
+                expect(gt.dnpgettext('messages', '', 'o2-1', 'o2-2', 2)).to.equal('t2-2');
             });
 
             it('should return singular match from selected context', function() {
-                expect(gt.dnpgettext('et_EE', 'c2', 'co2-1', 'co2-2', 1)).to.equal('ct2-1');
+                expect(gt.dnpgettext('messages', 'c2', 'co2-1', 'co2-2', 1)).to.equal('ct2-1');
             });
 
             it('should return plural match from selected context', function() {
-                expect(gt.dnpgettext('et_EE', 'c2', 'co2-1', 'co2-2', 2)).to.equal('ct2-2');
+                expect(gt.dnpgettext('messages', 'c2', 'co2-1', 'co2-2', 2)).to.equal('ct2-2');
             });
 
             it('should return singular match for non existing domain', function() {
-                expect(gt.dnpgettext('cccc', '', 'o2-1', 'o2-2', 1)).to.equal('o2-1');
+                expect(gt.dnpgettext('nonexisting', '', 'o2-1', 'o2-2', 1)).to.equal('o2-1');
             });
         });
 
@@ -125,7 +120,7 @@ describe('Gettext', function() {
 
         describe('#dgettext', function() {
             it('should return singular from default context', function() {
-                expect(gt.dgettext('et-ee', 'o2-1')).to.equal('t2-1');
+                expect(gt.dgettext('messages', 'o2-1')).to.equal('t2-1');
             });
         });
 
@@ -137,7 +132,7 @@ describe('Gettext', function() {
 
         describe('#dngettext', function() {
             it('should return plural from default context', function() {
-                expect(gt.dngettext('et-ee', 'o2-1', 'o2-2', 2)).to.equal('t2-2');
+                expect(gt.dngettext('messages', 'o2-1', 'o2-2', 2)).to.equal('t2-2');
             });
         });
 
@@ -149,7 +144,7 @@ describe('Gettext', function() {
 
         describe('#dpgettext', function() {
             it('should return singular from selected context', function() {
-                expect(gt.dpgettext('et-ee', 'c2', 'co2-1')).to.equal('ct2-1');
+                expect(gt.dpgettext('messages', 'c2', 'co2-1')).to.equal('ct2-1');
             });
         });
 
@@ -161,7 +156,7 @@ describe('Gettext', function() {
 
         describe('#getComment', function() {
             it('should return comments object', function() {
-                expect(gt.getComment('et-ee', '', 'test')).to.deep.equal({
+                expect(gt.getComment('messages', '', 'test')).to.deep.equal({
                     translator: 'Normal comment line 1\nNormal comment line 2',
                     extracted: 'Editors note line 1\nEditors note line 2',
                     reference: '/absolute/path:13\n/absolute/path:14',
